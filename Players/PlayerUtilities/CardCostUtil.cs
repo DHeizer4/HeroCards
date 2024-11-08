8e03ed4bb07b0056887fd04748e5ae3da92876f9
David Heizer <heizerdragons@gmail.com>
2024-11-08 13:08:39 -0500

    Cards cost now checked and paid

---
+++ Players/PlayerUtilities/CardCostUtil.cs
@@ -0,0 +1,58 @@
+﻿using Cards_Games.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static Cards_Games.Enumerations.CardResourceEnum;
+
+namespace Cards_Games.Players.PlayerUtilities
+{
+    class CardCostUtil
+    {
+        public static bool CanAfford(IRPGPlayer player, RPGCard card)
+        {
+            bool canAfford = true;
+
+            foreach(Cost cost in card.Costs)
+            {
+                if (cost.Resource == CardResource.Mana && player.Mana < cost.Amount)
+                {
+                    canAfford = false;
+                    break;
+                }
+                else if (cost.Resource == CardResource.Health &&  player.Health < cost.Amount)
+                {
+                    canAfford = false;
+                    break;
+                } 
+                else if (cost.Resource == CardResource.Time && player.Time < cost.Amount)
+                {
+                    canAfford = false;
+                    break;
+                }
+            }
+
+            return canAfford;
+        }
+
+        public static void PayCosts(IRPGPlayer player, RPGCard card)
+        {
+            foreach (Cost cost in card.Costs)
+            {
+                if (cost.Resource == CardResource.Mana)
+                {
+                    player.Mana -= cost.Amount;
+                }
+                else if (cost.Resource == CardResource.Health)
+                {
+                    player.Health -= cost.Amount;
+                }
+                else if (cost.Resource == CardResource.Time)
+                {
+                    player.Time -= cost.Amount;
+                }
+            }
+        }
+
+
+    }
+}
